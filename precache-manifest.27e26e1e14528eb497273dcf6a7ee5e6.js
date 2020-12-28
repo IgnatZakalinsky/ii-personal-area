29183380a7038aba22104fdea356344812c82c01
IgnatZakalinsky <ai73a@yandex.ru>
2020-12-28 16:32:10 +0300

    Updates

---
+++ precache-manifest.27e26e1e14528eb497273dcf6a7ee5e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fb127c9f9d5ae889f09f4c90bc18e5",
+    "url": "/ii-personal-area/index.html"
+  },
+  {
+    "revision": "442c4332556fa12ba9e7",
+    "url": "/ii-personal-area/static/css/2.816f14d9.chunk.css"
+  },
+  {
+    "revision": "042944c7592fb0b2a0c9",
+    "url": "/ii-personal-area/static/css/main.553bb218.chunk.css"
+  },
+  {
+    "revision": "442c4332556fa12ba9e7",
+    "url": "/ii-personal-area/static/js/2.f4c57b28.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/ii-personal-area/static/js/2.f4c57b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042944c7592fb0b2a0c9",
+    "url": "/ii-personal-area/static/js/main.0936c963.chunk.js"
+  },
+  {
+    "revision": "d2e7f39f7b5e431f19fa",
+    "url": "/ii-personal-area/static/js/runtime-main.8c54ad9d.js"
+  },
+  {
+    "revision": "7e6e1cf7ef72619dadc9708d5de76631",
+    "url": "/ii-personal-area/static/media/cosmos.7e6e1cf7.svg"
+  }
+]);
